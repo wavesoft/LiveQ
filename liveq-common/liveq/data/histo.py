4b710950f04b6a8b0f58a5858f54d1cdcca72355
wavesoft <johnys2@gmail.com>
2013-11-05 11:14:56 +0100

    Updated UI

--- liveq-common/liveq/data/histo.py
+++ liveq-common/liveq/data/histo.py
@@ -17,7 +17,11 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 ################################################################
 
+import logging
 import numpy
+import xml.etree.cElementTree as eTree
+
+from liveq.utils.FLAT import FLATParser
 
 class HistogramCollection:
 	"""
@@ -182,16 +186,16 @@ class Histogram:
 	and with the same width.
 	"""
 	
-	def __init__(self, bins=10, bininfo=[], y=None, yErrPlus=None, yErrMinus=None):
+	def __init__(self, name="", bins=10, y=None, yErrPlus=None, yErrMinus=None, x=None, xErrPlus=None, xErrMinus=None):
 		"""
 		Basic histogram representation
 		"""
 
-		# Bin position and width info (static)
+		# Store bin info
 		self.bins = bins
-		self.bininfo = bininfo
+		self.name = name
 
-		# Initialize bins
+		# Initialize bin values
 		if y == None:
 			y = numpy.zeros(bins)
 		if yErrPlus == None:
@@ -199,7 +203,376 @@ class Histogram:
 		if yErrMinus == None:
 			yErrMinus = numpy.zeros(bins)
 
+		# Initialize bins
+		if x == None:
+			x = numpy.zeros(bins)
+		if xErrPlus == None:
+			xErrPlus = numpy.zeros(bins)
+		if xErrMinus == None:
+			xErrMinus = numpy.zeros(bins)
+
 		# Store values
 		self.y = y
 		self.yErrPlus = yErrPlus
 		self.yErrMinus = yErrMinus
+		self.x = x
+		self.xErrPlus = xErrPlus
+		self.xErrMinus = xErrMinus
+
+	def copy(self):
+		"""
+		Create a copy of this histogram
+		"""
+
+		# Create new object with cloned info
+		return Histogram(
+				name=self.name,
+				bins=self.bins,
+				y=numpy.copy(self.y),
+				yErrPlus=numpy.copy(self.yErrPlus),
+				yErrMinus=numpy.copy(self.yErrMinus),
+				x=numpy.copy(self.x),
+				xErrPlus=numpy.copy(self.xErrPlus),
+				xErrMinus=numpy.copy(self.xErrMinus)
+			)
+
+	def isNormalized(self):
+		"""
+		Check if the specified histogram is normalized.
+		"""
+
+		# Check if the sum is "close to" 1
+		tot = numpy.sum(self.y)
+
+		# Return
+		return (tot >= 0.9999) and (tot <= 1.00001)
+
+
+	def normalize(self, copy=True):
+		"""
+		Normalize the y-values so they sum to 1.
+		"""
+		# NOTE: I have no idea what I am doing
+		# TODO: Read about this
+
+		# Calculate current sum
+		tot = numpy.sum(self.y)
+
+		# Calculate scale (also equal to y-Values)
+		self.y = self.y / tot
+
+		# Update errors
+		self.yErrMinus *= self.y
+		self.yErrPlus *= self.y
+
+	def sumArea(self):
+		"""
+		Return the summarized area of the histogram
+		"""
+		
+		# Get bin's xerr
+		binW = (self.xErrMinus + self.xErrPlus)/2
+
+		# Calc bin areas
+		binArea = (binW * self.y)
+
+		# Return sum
+		return numpy.sum(binArea)
+
+	@staticmethod
+	def merge(self, *args):
+		"""
+		Merge the statistics with the specified histogram.
+		"""
+
+	def pack(self):
+		"""
+		Return a packed representation of the histogram that
+		can be unpacked from the javascript client.
+		"""
+
+	def chi2ToReference(self, refHisto, uncertainty=0.05):
+		"""
+		Calculate the chi-squared between the current histogram
+		and the given (reference) histogram in the specifeid uncertainty.
+		"""
+
+		# Validate binsize
+		if refHisto.bins != self.bins:
+			raise ValueError("The specified reference histogram does not have the same bin size!")
+
+		# Prepare vars
+		Chi2 = 0;
+		N = 0;
+
+		# handle bins
+		for i in range(0, self.bins):
+
+			# Require same bins filled. If data is filled and MC is not filled,
+			# we do not know what the chi2 of that bin is. Return error.
+			# (b.isEmpty() && !r.isEmpty()) return -1;
+			if (self.y[i] == 0) and (refHisto.y[i] == 0):
+				return -11
+
+			# Skip empty bins (if data is empty but theory is filled, it's ok. We
+			# are allowed to plot theory outside where there is data, we just 
+			# cannot calculate a chi2 there).
+			if (self.y[i] == 0) or (refHisto.y[i] == 0):
+				continue
+
+			# compute one element of test statistics:
+			#                     (Theory - Data)^2
+			# X = --------------------------------------------------------
+			#      Sigma_data^2 + Sigma_theory^2 + (uncertainty*Theory)^2
+
+			Theory = self.y[i]
+			Data = refHisto.y[i]
+
+			if Theory > Data:
+				Sigma_theory = self.yErrMinus[i]
+				Sigma_data = refHisto.yErrPlus[i]
+			else:
+				Sigma_theory = self.yErrPlus[i]
+				Sigma_data = refHisto.yErrMinus[i]
+
+			nomin = (Theory - Data) * (Theory - Data)
+			denom = Sigma_data * Sigma_data + Sigma_theory * Sigma_theory + (uncertainty*Theory) * (uncertainty*Theory)
+
+			if denom == 0:
+				raise ValueError("Unexpected division by zero!")
+
+			X = nomin/denom
+
+			Chi2 += X
+			N += 1
+
+		# TODO: Calculate NDOF properly (decrease by 1) if histograms
+		#       area was normalized to the constant
+		if N == 0:
+			raise ValueError("No bins to compare!")
+
+		return Chi2/N
+
+	@staticmethod
+	def fromFLAT(filename):
+		"""
+		Create a histogram by reading the specified FLAT file
+		"""
+
+		# Parse into structures
+		data = FLATParser.parse(filename)
+
+		# Ensure we got at least a HISTOGRAM
+		if not 'HISTOGRAM' in data:
+			return None
+
+		# Get some metrics
+		vBins = data['HISTOGRAM']['v']
+
+		numBins = len(vBins)
+		numValues = len(vBins[0])
+
+		# Convert values into a flat 2D numpy array
+		values = numpy.array(vBins, dtype=numpy.float64).flatten()
+		name = data['HISTOGRAM']['d']['AidaPath']
+
+		# If we have:
+		# 6 values we have : xLow, xFocus, xHigh, y, yErrMin, yErrMax
+		# 5 values we have : xLow, xHigh, y, yErrMin, yErrMax
+
+		if numValues == 6:
+			# Extract parts and build histogram
+			return Histogram(
+					bins=numBins,
+					name=name,
+					xErrMinus=values[1::6]-values[::6],
+					x=values[1::6],
+					xErrPlus=values[2::6]-values[1::6],
+					y=values[3::6],
+					yErrMinus=values[4::6],
+					yErrPlus=values[5::6]
+				)
+
+		elif numValues == 6:
+			# Calculate xMid
+			xMid = (values[::5] + values[1::5]) / 2.0
+
+			# Extract parts and build histogram
+			return Histogram(
+					bins=numBins,
+					name=name,
+					x=xMid,
+					xErrMinus=xMid-values[::5],
+					xErrPlus=values[1::5]-xMid,
+					y=values[2::5],
+					yErrMinus=values[3::5],
+					yErrPlus=values[4::5]
+				)
+
+
+	@staticmethod
+	def fromAIDA(filename, setName=None):
+		"""
+		Create a histogram by reading the specified AIDA file
+		"""
+
+		# Load tree from XML
+		tree = eTree.parse(filename)
+		root = tree.getroot()
+
+		# Allocate measurement arrays
+		bins = 0
+		x = [ ]
+		xErrPlus = [ ]
+		xErrMinus = [ ]
+		y = [ ]
+		yErrPlus = [ ]
+		yErrMinus = [ ]
+
+		# Process data sets
+		for dpSet in root:
+			if dpSet.tag == 'dataPointSet':
+
+				# Get attrib
+				sName = dpSet.attrib['name']
+				sDim = dpSet.attrib['dimension']
+				sPath = dpSet.attrib['path']
+				sTitle = dpSet.attrib['title']
+
+				# Look for set name
+				if (setName == None) or (setName == sName):
+					print "Importing %s" % sName
+				
+					# Process data points
+					for dPoint in dpSet:
+						if dPoint.tag == 'dataPoint':
+
+							# Reset dimention index
+							dimIndex = 0
+
+							# Count number of bins
+							bins += 1
+
+							# Process measurements
+							for dMeasurement in dPoint:
+								if dMeasurement.tag == 'measurement':
+
+									# Get values
+									v = numpy.float64(dMeasurement.attrib['value'])
+									eMinus = numpy.float64(dMeasurement.attrib['errorMinus'])
+									ePlus = numpy.float64(dMeasurement.attrib['errorPlus'])
+
+									# Store x-values (first dimention)
+									if dimIndex == 0:
+										x.append(v)
+										xErrMinus.append(eMinus)
+										xErrPlus.append(ePlus)
+
+									# Store y-values (second dimention)
+									elif dimIndex == 1:
+										y.append(v)
+										yErrMinus.append(eMinus)
+										yErrPlus.append(ePlus)
+
+									# Change dimention
+									dimIndex += 1
+
+					# Exit data set loop
+					break
+
+		# Create and return histogram
+		return Histogram(
+				name="%s/%s" % (sPath, sName),
+				bins=bins,
+				y=numpy.array(y),
+				yErrMinus=numpy.array(yErrMinus),
+				yErrPlus=numpy.array(yErrPlus),
+				x=numpy.array(y),
+				xErrMinus=numpy.array(xErrMinus),
+				xErrPlus=numpy.array(xErrPlus)
+			)
+
+	@staticmethod
+	def allFromAIDA(filename):
+		"""
+		Create a dictionary of histograms by reading all the dataPointSets in the AIDA file.
+		"""
+
+		# Load tree from XML
+		tree = eTree.parse(filename)
+		root = tree.getroot()
+
+		# Prepare the response dict
+		ans = { }
+
+		# Process data sets
+		for dpSet in root:
+			if dpSet.tag == 'dataPointSet':
+
+				# Reset measurement arrays
+				bins = 0
+				x = [ ]
+				xErrPlus = [ ]
+				xErrMinus = [ ]
+				y = [ ]
+				yErrPlus = [ ]
+				yErrMinus = [ ]
+
+				# Get attrib
+				sName = dpSet.attrib['name']
+				sDim = dpSet.attrib['dimension']
+				sPath = dpSet.attrib['path']
+				sTitle = dpSet.attrib['title']
+
+				print "Importing %s" % sName
+			
+				# Process data points
+				for dPoint in dpSet:
+					if dPoint.tag == 'dataPoint':
+
+						# Reset dimention index
+						dimIndex = 0
+
+						# Count number of bins
+						bins += 1
+
+						# Process measurements
+						for dMeasurement in dPoint:
+							if dMeasurement.tag == 'measurement':
+
+								# Get values
+								v = numpy.float64(dMeasurement.attrib['value'])
+								ePlus = numpy.float64(dMeasurement.attrib['errorPlus'])
+								eMinus = numpy.float64(dMeasurement.attrib['errorMinus'])
+
+								# Store x-values (first dimention)
+								if dimIndex == 0:
+									x.append(v)
+									xErrMinus.append(eMinus)
+									xErrPlus.append(ePlus)
+
+								# Store y-values (second dimention)
+								elif dimIndex == 1:
+									y.append(v)
+									yErrMinus.append(eMinus)
+									yErrPlus.append(ePlus)
+
+								# Change dimention
+								dimIndex += 1
+
+				# Store dataset
+				name = "%s/%s" % (sPath, sName)
+				ans[name] = Histogram(
+					name=name,
+					bins=bins,
+					y=numpy.array(y),
+					yErrMinus=numpy.array(yErrMinus),
+					yErrPlus=numpy.array(yErrPlus),
+					x=numpy.array(y),
+					xErrMinus=numpy.array(xErrMinus),
+					xErrPlus=numpy.array(xErrPlus)
+				)
+
+		# Create and return histogram
+		return ans
+
--- liveq-webserver/static/interface.js
+++ liveq-webserver/static/interface.js
@@ -7,47 +7,119 @@
 	/**
 	 * A job object that fires events when it's changed
 	 */
-	function Knob(cfg) {
-		var cfg = cfg || { };
+	function Knob(config) {
+		var cfg = config || { };
 		this.width = cfg.width || 100;
-		this.height = cfg.height || 100;
+		this.height = cfg.height || 120;
+		this.fgColor = cfg.fgColor || "#87CEEB";
+		this.title = cfg.title || "Knob";
+		this.lblColor = cfg.lblColor || "#000000";
+		this.titleHeight = cfg.titleHeight || 20;
+		this.min = (cfg.min == undefined) ? 0 : cfg.min;
+		this.max = (cfg.max == undefined) ? 10 : cfg.max;
+		this.steps = (cfg.steps == undefined) ? 10 : cfg.steps;
+		this.step = (cfg.step == undefined) ? 0.1 : cfg.step;
+		this.decimals = (cfg.decimals == undefined) ? 2 : cfg.decimals;
+		this.value = cfg.value || this.min;
 
 		this.element = $('<div />');
-		this.dial = $('<input class="dial" />');
-		this.label = $('<div class="dialLabel" />');
+		this.dial = $('<input />');
+		this.lblValue = $('<div />');
+		this.lblTitle = $('<div />');
+		this.btnHelp = $('<span class="glyphicon glyphicon-question-sign" />');
+
+		// Nest everything
+		$(this.element).append(this.lblValue);
+		$(this.element).append(this.dial);
+		$(this.element).append(this.lblTitle);
 
 		$(this.element).css({
-			'display': 'inline-block',
+			'display': 'block',
 			'width': this.width,
 			'height': this.height,
-			'position': 'relative'
+			'position': 'relative',
+			'float': 'left',
+			'margin': 8
 		});
 
-		$(this.label).css({
+		$(this.lblValue).css({
 			'position': 'absolute',
 			'left': 0,
 			'right': 0,
-			'top': this.height/2 - 10,
-			'font-size': 12,
-			
+			'top': (this.height- this.titleHeight)/2 - 10,
+			'font-size': 14,
+			'color': this.fgColor,
+			'text-align': 'center'
 		});
+
+		$(this.lblTitle).css({
+			'display': 'block',
+			'width': this.width,
+			'height': this.titleHeight,
+			'font-size': Math.round(this.titleHeight / 1.2),
+			'text-align': 'center',
+			'color': this.lblColor,
+			'text-align': 'center'
+		});
+
+		$(this.btnHelp).css({
+			'position': 'absolute',
+			'right': '0',
+			'width': 16,
+			'height': 16,
+			'cursor': 'pointer'
+		});
+
+		$(this.dial).mouseover((function(){
+			$(this.btnHelp).show();
+		}).bind(this));
+
+		$(this.dial).mouseout((function(){
+			$(this.btnHelp).hide();
+		}).bind(this));
+
+
+		// Re-map to integers
+		var vrange = (this.max - this.min);
+		var range = Math.round(vrange / this.step);
+		var stepSize = Math.round(range / this.steps);
+		console.log(vrange, range, this.step, stepSize, this.steps);
+
+		$(this.lblValue).html( Number(this.value).toFixed(this.decimals) );
+		$(this.lblTitle).html( this.title );
+		$(this.lblTitle).append(this.btnHelp);
+
+		$(this.dial).knob({
+            'min':0,
+            'max': range,
+            'step': stepSize,
+            'width': this.width,
+            'height': this.height - this.titleHeight,
+            'displayInput': false,
+
+            'change': (function(v) {
+            	if (v < 0) v = 0;
+            	if (v > range) v=range;
+				var value = vrange * v / range + this.min;
+				console.log(v, '->', value, this.decimals);
+				$(this.lblValue).html(value.toFixed(this.decimals))
+            }).bind(this)
+
+		});
+
 	}
 
-	function makeKnob() {
-		var container = $("#panel-tune");
-		var h = $('<div />'),
-			e = $('<input class="dial" />'),
-			d = $('<div class="dialLabel" />');
-
-		container.append(e);
-		$(e).knob({
-            'min':0,
-            'max':100,
-            'step': 1,
-            'width': 100,
-            'height': 100,
-            'displayInput': false
-		});
+	function makeKnob(title, min, max) {
+		var container = $("#panel-tune"),
+			knob = new Knob({
+				'title': title,
+				'min': min,
+				'max': max,
+				'step': 0.05,
+				'decimals': 2
+			});
+		container.append(knob.element);
+		return container;
 	}
 
 	function resetTuneViews() {
@@ -92,7 +164,9 @@
 	};
 
 	var initUI = glob.initUI = function() {
-		makeKnob();
+		makeKnob("AlphaS", 0, 100);
+		makeKnob("Volumetric", 0, 100);
+		makeKnob("Test", 0, 100);
 	};
 
 
