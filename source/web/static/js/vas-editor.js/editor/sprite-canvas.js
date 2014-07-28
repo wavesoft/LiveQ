bf34927579d5e0bff71c1ae4193ab72748663651
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2014-07-28 14:13:23 +0200

    Working on timeline

--- source/web/static/js/vas-editor.js/editor/sprite-canvas.js
+++ source/web/static/js/vas-editor.js/editor/sprite-canvas.js
@@ -4,6 +4,71 @@ define(
 
 	function($, fabric, createjs, DB) {
 
+		var ObjectWrapper = function( object ) {
+			this.__object = object;
+
+			// Extract/create some additional properties
+			this.__object.__pathProgression = 1;
+			this.__object.__pathElements = this.__object.path.slice(0);
+
+			// OnUpdate handler
+			this.onUpdate = null;
+
+			// Prepare property config
+			var config = {
+				"progression": {
+					get: (function() {
+
+						return this.__object.__pathProgression;
+
+					}).bind(this),
+					set: (function(v) {
+						
+						// Wrap V in bounds
+						if (v<0) v=0;
+						if (v>1) v=1;
+
+						// Calculate the path progression
+						var pathLen = this.__object.__pathElements.length,
+							pathPos = parseInt( v * (pathLen-1) );
+
+						// Update path according to path progression
+						this.__object.path = this.__object.__pathElements.slice(0, pathPos);
+
+						// Keep reference
+						this.__object.__pathProgression = v;
+
+						// Fire the onUpdate handler
+						if (this.onUpdate)
+							this.onUpdate("progression", v);
+
+					}).bind(this)
+				}
+			};
+
+			// Passthrough properties
+			var passthrough = [ "left", "top", "scaleX", "scaleY" ];
+			for (var i=0; i<passthrough.length; i++) {
+				config[passthrough[i]] = {
+					get: (function(propName) { 
+						return function() {
+							return this.__object[propName];
+						}
+					})(passthrough[i]).bind(this),
+					set: (function(propName) { 
+						return function(value) {
+							this.__object[propName] = value;
+							if (this.onUpdate) this.onUpdate();
+						}
+					})(passthrough[i]).bind(this)
+				};
+			}
+
+			// Define properties using the config
+			Object.defineProperties(this, config);
+
+		}
+
 		var SpriteCanvas = function(store, canvasElement) {
 			this.store = store;
 			this.canvasElement = canvasElement;
@@ -18,6 +83,33 @@ define(
 			canvas.freeDrawingBrush.width = 4;
 
 			window.c = canvas;
+
+			canvas.on('path:created', (function(obj) {
+
+				console.log(obj);
+				canvas.isDrawingMode = false;
+
+				window.p = obj.path;
+
+				window.ow = new ObjectWrapper(obj.path);
+				window.ow.onUpdate = function() { canvas.renderAll(); };
+
+				window.doit = function() {
+					window.ow.progression = 0;
+
+					window.timeline = new createjs.Timeline();
+
+					var tween = createjs.Tween.get(window.ow)
+							.wait(500)
+							.to({progression:1}, 1000);
+
+					window.timeline.addTween(tween);
+					setInterval(function() {
+						window.timeline.tick(10);
+					}, 10);
+				}
+
+			}).bind(this));
 
 		};
 
