47eead63d560c413973dfd8841c7ba562fc952e9
Ioannis Charalampidis <wavesoft@github.com>
2014-08-08 12:09:02 +0200

    Fixing histogram

--- source/web/static/js/vas-basic.js/dataviz/histogram.js
+++ source/web/static/js/vas-basic.js/dataviz/histogram.js
@@ -23,8 +23,17 @@ define(
 				'left': 40,
 				'top': 10,
 				'bottom': 10,
-				'right': 10
+				'right': 10,
+				'title': 5
 			};
+			this.legend = {
+				'bullet': 5,
+				'height': 16,
+				'titleMargin': 5,
+			};
+			this.errorBars = {
+				'bulletSize' : 2
+			}
 
 			// Prepare histogram SVG host
 			this.svg = d3.select( this.hostDOM[0] )
@@ -32,6 +41,8 @@ define(
 						     .attr("class", "dv-plot dv-invert");
 			this.svgPlot = this.svg.append("g")
 							 .attr("transform", "translate("+this.margin.left+","+this.margin.top+")");
+			this.svgLegend = this.svgPlot.append("g")
+							.attr("class", "legend");
 
 		};
 
@@ -51,6 +62,57 @@ define(
 			} else {
 				return this.yScale(y);
 			}
+		}
+
+		/**
+		 * Calculate how many points are inside each region and find
+		 * the one with the smallest number of points.
+		 *
+		 * This function will return the index of the region array specified.
+		 *
+		 * @param {array} areas - The array of the region boundaries to check (format: [x,y,w,h])
+		 */
+		DataVizHistogram.prototype.getLeastUsedRegion = function(areas, plots) {
+
+			// Initialize usage array
+			var usage = [ ];
+			for (var i=0; i<areas.length; i++) {
+				usage.push(0);
+			}
+
+			// Iterate over the histograms
+			for (var j=0; j<plots.length; j++) {
+				var histo = plots[j].data;
+				// Run over bins and get point positions
+				for (var i=0; i<histo.length; i++) {
+					// Get position
+					var x = this.xScale(histo[i][3]),
+						y = this.yScaleNonZero(histo[i][0]);
+
+					// Hit-test areas
+					for (var k=0; k<areas.length; k++) {
+						if ( (x >= areas[k][0]) && (x <= (areas[k][0]+areas[k][2])) &&
+							 (y >= areas[k][1]) && (y <= (areas[k][1]+areas[k][3])) ) {
+							usage[k] += 1;
+							break;
+						}
+					}
+				}
+			}
+
+
+			// Find minimum
+			var min=usage[0], min_index=0;
+			for (var i=0; i<usage.length; i++) {
+				if (usage[i]<min) {
+					min=usage[i];
+					min_index=i;
+				}
+			}
+
+			// Return index
+			return min_index;
+
 		}
 
 		/**
@@ -131,6 +193,10 @@ define(
 				for (var j=0; j<plots.length; j++) {
 					var plot = plots[j];
 
+					// =============
+					//     Plots 
+					// =============
+
 					// Access plot's group
 					var group = this.svgPlot.select("g.plot#"+plot.id);
 					if (group.empty()) {
@@ -157,6 +223,49 @@ define(
 					record.exit()
 						.remove();
 
+					// =============
+					//  Data Points
+					// =============
+
+					var record = group.selectAll("path.plot-bullet")
+									.data(plot.data);
+
+					// Enter
+					record.enter()
+						.append("svg:path")
+							.attr("class", "plot-bullet")
+							.attr("stroke", plot.color);
+
+					// Update
+					record
+						.attr("transform", function(d,i){
+							return "translate(" + self.xScale(d[3]) + "," + self.yScaleNonZero(d[0]) + ")"
+						})
+						.attr("d", function(d,i) {
+							var bs = self.errorBars.bulletSize/2;
+
+							// Path for the bullet
+							var D_RECT= "M"+(-bs)+","+(-bs)+
+										"L"+bs+","+(-bs)+
+										"L"+bs+","+bs+
+										"L"+(-bs)+","+bs+
+										"Z";
+
+							// Calculate error bar sizes
+							var e_up = self.yScaleNonZero(d[0]) - self.yScaleNonZero(d[0]+d[1]),
+								e_dn = self.yScaleNonZero(d[0]-d[2]) - self.yScaleNonZero(d[0]);
+
+							// Draw upper error bar
+							var D_YEUP = "M"+(-bs)+","+(-e_up)+"L"+bs+","+(-e_up)+"Z" +
+										 "M0,"+(-e_up)+"L0,"+(-bs)+"Z";
+							var D_YEDN = "M"+(-bs)+","+e_dn+"L"+bs+","+e_dn+"Z" +
+										 "M0,"+e_dn+"L0,"+bs+"Z";
+
+							return  D_YEUP+D_YEDN + D_RECT;
+
+						})
+						.attr("stroke", plot.color);
+
 				}
 
 				// ------------------------------------------
@@ -175,6 +284,106 @@ define(
 				// Render them
 				this.xAxisGraphic.call(this.xAxis);
 				this.yAxisGraphic.call(this.yAxis);
+
+				// ------------------------------------------
+				//  Graphic Design - Legend
+				// ------------------------------------------
+
+				//
+				// Find in which region to place the legend.
+				// The placeAt gets one of the following values:
+				//
+				//  +---+---+
+				//  | 0 | 1 |
+				//  +---+---+
+				//  | 2 | 3 |
+				//  +---+---+
+				//
+				var midW = width/2, midH = height/2,
+					lb = this.legend.bullet,
+					placeAt = this.getLeastUsedRegion([
+							[0,0,midW,midH], [midW,0,midW,midH],
+							[0,midH,midW,midH], [midW,midH,midW,midH]
+						], plots);
+
+				// Calculate offsets, based on the area we should place the legend
+				var rightAlign = false, ofsX = 0, ofsY = 0, yDirection = 1;
+				if (placeAt == 0) {
+					ofsX = this.legend.titleMargin*2 + 20;
+					ofsY = 20;
+				} else if (placeAt == 1) {
+					rightAlign = true;
+					ofsX = width;
+					ofsY = 20;
+				} else if (placeAt == 2) {
+					yDirection = -1;
+					ofsX = this.legend.titleMargin*2 + 20;
+					ofsY = height - this.legend.titleMargin*2 - 20;
+				} else if (placeAt == 3) {
+					rightAlign = true;
+					yDirection = -1;
+					ofsX = width;
+					ofsY = height - this.legend.titleMargin*2 - 20;
+				}
+
+				// Set legend alignment
+				this.svgLegend
+					.attr("transform", "translate("+ofsX+","+ofsY+")");
+
+				// Open data record
+				var record = this.svgLegend.selectAll("g.legend-entry").data(plots);
+
+				// Enter
+				var newGroups = record.enter()
+					.append("g")
+						.attr("class", "legend-entry");
+					if (rightAlign) {
+						newGroups.append("path")
+							.attr("d", "M"+(-lb)+","+(-lb/2)
+									  +"L0,"+(-lb/2)
+									  +"L0,"+(lb/2)
+									  +"L"+(-lb)+","+(lb/2)
+									  +"Z" );
+						newGroups.append("text")
+							.attr("transform", "translate("+(-lb-4)+",0)")
+							.style("dominant-baseline", "middle")
+							.style("text-anchor", "end" );
+					} else {
+						newGroups.append("path")
+							.attr("d", "M"+lb+","+(-lb/2)
+									  +"L0,"+(-lb/2)
+									  +"L0,"+(lb/2)
+									  +"L"+lb+","+(lb/2)
+									  +"Z" );
+						newGroups.append("text")
+							.attr("transform", "translate("+(lb+4)+",0)")
+							.style("dominant-baseline", "middle")
+							.style("text-anchor", "start" );
+					}
+
+				// Update
+				record
+					.attr("transform", function(d,i) { 
+						return "translate(0,"+(yDirection*i*self.legend.height)+")"
+					});
+				record.selectAll("text")
+					.text(function(d,i) { return d.legend + (d.data.interpolated?" [Estimate]":"") });
+				record.selectAll("path")
+					.attr("fill", function(d,i) { 
+						var color = d.color;
+						if (typeof(d.color) == 'object') {
+							if (d.data.interpolated) {
+								color = d.color[1];
+							} else {
+								color = d.color[0];
+							}
+						}
+						return color; 
+					});
+
+				// Delete
+				record.exit()
+					.remove();
 
 			}
 
