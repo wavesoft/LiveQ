ef82583b302fb7de6ba421a17e7a5a7e112e0073
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-03-11 14:00:20 +0100

    Flushing output file on write

--- tools/analyze-mcplots-jobs.py
+++ tools/analyze-mcplots-jobs.py
@@ -177,6 +177,7 @@ if __name__ == '__main__':
 			# In case of successful processing, log line
 			if result == ".":
 				csvFile.write("%s\n" % result[1:])
+				csvFile.flush()
 
 			# Display progress every once in a while
 			numCompleted += 1
