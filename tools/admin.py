e0d36e6d00c0d3aeedd1274475b9f87a874648cd
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-03-26 17:41:09 +0100

    Working on a super-admin interface

---
+++ tools/admin.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+################################################################
+# LiveQ - An interactive volunteering computing batch system
+# Copyright (C) 2013 Ioannis Charalampidis
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+################################################################
+
+# This script imports all Tunables from a Pythia8 installation
+
+# ----------
+import os
+import sys
+sys.path.append("%s/liveq-common" % os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append("%s/liveq-webserver" % os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+# ----------
+
+import argparse
+import json
+import os
+import util.pythia as pythia
+from util.config import Config
+
+from liveq import handleSIGINT, exit
+from liveq.exceptions import ConfigException
+
+from liveq.models import *
+from webserver.models import *
+
+# Prepare runtime configuration
+runtimeConfig = { }
+
+# Load configuration
+try:
+	Config.fromFile( "config/common.conf.local", runtimeConfig )
+except ConfigException as e:
+	print("ERROR   Configuration exception: %s" % e)
+	exit(1)
+
+# Hook CTRL+C
+handleSIGINT()
+
+# Prepare argument parser
+parser = argparse.ArgumentParser(prog='admin.py', description='LiveQ Administration Command-Line interface.')
+parser.add_argument('command', type=str, nargs='?',
+                   help='the command to process')
+args = parser.parse_args()
+
+# Dispatch command
+if args.command == "deluser"
+
+
