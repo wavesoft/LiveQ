d349ce6669b0a1e992527a1c72b6aec724154cc4
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-04-17 11:27:36 +0900

    Using env bootstrap

--- tools/schema-upgrade.py
+++ tools/schema-upgrade.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 ################################################################
 # LiveQ - An interactive volunteering computing batch system
 # Copyright (C) 2013 Ioannis Charalampidis
